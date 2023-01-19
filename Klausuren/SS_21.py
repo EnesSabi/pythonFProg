e1156ec0bc88000574d0ee29ad2ebd8a505ec30c
EnesSabi <104065837+EnesSabi@users.noreply.github.com>
2023-01-19 17:22:10 +0100

    exams-finished

---
+++ Klausuren/SS_21.py
@@ -0,0 +1,52 @@
+# Klausur aus dem Sommersemester 2021
+## Aufgabe 1) Datentypen und Variable
+a = 11 // 3 / 2
+b = 2e+1j # -> mathematische Schreibweise real+imag(j) und 2e5 -> aex = a*10^x
+c = [1,2] + [(3,4)]
+d = (3,5,7,9)[::-1]
+#Loesung
+print("type = ", type(a) , ", value = " , a)
+print("type = ", type(b) , ", value = " , b)
+print("type = ", type(c) , ", value = " , c)
+print("type = ", type(d) , ", value = " , d)
+
+## Aufgabe 2) Listen und Tupel
+# a) Definieren Sie eine Liste von Tripel (3er Tupeln), die alle Möglichkeiten darstellt, wie man drei Kugeln mit 1, 2 und 3 beschriftet aus einer Urne ziehen kann.
+triplets = [(i, j, k) for i in [1, 2, 3] for j in [1, 2, 3] for k in [1, 2, 3]]
+#Lösung
+print(triplets)
+
+# b) Schreiben Sie einen lambda-Ausdruck, der eine zweistellige Funktion (mit zwei Parametern x und y) darstellt, deren Rückgabewert eine Liste aller geraden Zahlen zwischen x und y ist. Damit soll folgende Ausgabe erzeugt werden:
+
+z = list(map(lambda x, y: [i for i in range(x, y+1) if not i % 2], [1,3],[7,9]))
+print(z)
+
+#Schreiben Sie eine Python-Klasse Messwert. Diese soll folgende Elemente enthalten:
+#a) Eine private Variable wert vom Typ float
+#b) Einen allgemeinen Konstruktor, mit dem der Wert mit einem übergebenen Parameter
+#initialisiert werden kann
+#c) Eine Getter- und eine Setter-Methode für den Wert als Property.
+#d) Eine Methode __str__() für Print-Ausgaben.
+#e) Ein Testskript, das alle Methoden der Klasse nutzt und mit sinnvollen Werten aufruft
+
+class Messwert:
+    def __init__(self, wert):
+        self.__wert = wert
+
+    @property
+    def wert(self):
+        return self.__wert
+
+    @wert.setter
+    def wert(self, value):
+        self.__wert = value
+
+    def __str__(self):
+        return str(self.__wert)
+
+m = Messwert(5.5)
+print(m.wert) # 5.5
+m.wert = 7.5
+print(m.wert) # 7.5
+print(str(m)) # 7.5
+
---
+++ Klausuren/WS_20_21.py
@@ -0,0 +1,55 @@
+# Klausur aus dem Wintersemester 20/21
+## Aufgabe 1) Datentypen und Variable
+a = 9 // 2 / 2
+b = (~3 + 1) << 2
+c = [k % 2 for k in range(5)]
+d = [a + a for a in "abc"]
+#Loesung
+print("type = ", type(a) , ", value = " , a)
+print("type = ", type(b) , ", value = " , b)
+print("type = ", type(c) , ", value = " , c)
+print("type = ", type(d) , ", value = " , d)
+
+## Aufgabe 2) Listen und Tupel
+# Gegeben sei eine Liste q mit Zahlen. Erstellen Sie eine neue Liste, die nur diejenigen Elemente aus q enthält, die sowohl gerade Zahlen sind als auch an einer geraden Position (Index) in der Liste stehen
+q = [1,3,5,8,10,13,18,36,78]
+# Lösung -> [10,18,78]
+q1 = [q[i] for i in range(len(q)) if i % 2 == 0 and q[i] % 2 == 0]
+print(q1)
+
+## Aufgabe 3)
+# Schreiben Sie einen Lambda-Ausdruck, der eine Funktion mit Parameter n darstellt, deren Rückgabewert eine Liste aller Vielfachen von 3 zwischen 1 und n ist. Damit soll folgende Ausgabe erzeugt werden
+
+z = list(map(lambda i: [x for x in range(i) if x % 3 == 0], [13, 7]))
+print(z)
+
+## Aufgabe 4) Klassen und dynamische Datenstrukturen
+#Es soll eine Python-Klasse implementiert werden, die einen Stack repräsentiert. Diese soll
+#über folgende Inhalte verfügen:
+#a) Ein privates Attribut vom Typ list, das die Inhalte des Stacks verwaltet
+#b) Ein Konstruktor zur Initialisierung mit einer leeren Liste
+#c) Eine Methode push(), um einen Eintrag in den Stack einzufügen
+#d) Eine Methode pop(), die den letzten Eintrag wieder ausgibt und ihn aus der Liste
+#löscht (mit dem Befehl del)
+#e) Eine Methode empty(), die prüft, ob Elemente im Stack vorhanden sind
+
+class Stack:
+    def __init__(self):
+        self.__stack = []
+    def push(self, item):
+        self.__stack.append(item)
+    def pop(self):
+        if not self.empty():
+            return self.__stack.pop()
+        else:
+            print("Stack is empty")
+            return None
+    def empty(self):
+        return len(self.__stack) == 0
+
+wort = input("Wort: ")
+stapel = Stack()
+for zeichen in wort:
+    stapel.push(zeichen)
+while not stapel.empty():
+    print(stapel.pop(), end= " ")
